f2b33ac1c241f77b11adcfba5788c662cf1e7fa8
Shenuri Buddika <shenuribuddika@gmail.com>
2026-06-20 13:38:41 +0530

    Modern Portfolio

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log("Portfolio Loaded Successfully");
+const text = [
+    "IT Undergraduate",
+    "Web Developer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing").textContent = letter;
+
+    if(letter.length === currentText.length){
+
+        count++;
+        index = 0;
+
+        setTimeout(type,1000);
+
+    }else{
+
+        setTimeout(type,100);
+
+    }
+
+})();
+
+const topBtn = document.getElementById("topBtn");
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+
+});
